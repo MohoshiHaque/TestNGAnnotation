9ffae3fa4ee4d5eae7fb3bbea7f8e0e06beff4ab
mohoshi.haque <mohoshihaque@gmail.com>
2022-10-24 15:52:21 +0600

    TestNGAnnotation update-3

--- src/main/java/org/example/TestNGBasics.java
+++ src/main/java/org/example/TestNGBasics.java
@@ -6,45 +6,55 @@ public class TestNGBasics {
 
     @BeforeSuite
     public void setup1(){
-        System.out.println("Mohoshi Process-1");
+        System.out.println("Before Suite-1");
     }
 
     @BeforeClass
     public void setup2(){
-        System.out.println("Mohoshi Process-2");
+        System.out.println("Before Class-1");
     }
 
     @BeforeMethod
     public void setup3(){
-        System.out.println("Mohoshi Process-3");
+        System.out.println("Before Method-1");
+    }
+
+    @BeforeMethod
+    public void setup3_1(){
+        System.out.println("Before Method-2");
     }
 
     @Test
     public void setup4(){
-        System.out.println("Mohoshi Process-4");
+        System.out.println("Test-1");
+    }
+
+    @Test
+    public void setup4_3(){
+        System.out.println("Test-3");
     }
 
     @AfterMethod
     public void setup5(){
-        System.out.println("Mohoshi Process-5");
+        System.out.println("After Method-1");
     }
 
     @AfterClass
     public void setup6(){
-        System.out.println("Mohoshi Process-6");
+        System.out.println("After Class-1");
     }
 
     @AfterSuite
     public void setup7(){
-        System.out.println("Mohoshi Process-7");
+        System.out.println("After Suite-1");
     }
     @Test
-    public void setup8(){
-        System.out.println("Mohoshi Process-8");
+    public void setup4_2(){
+        System.out.println("Test-2");
     }
     @AfterMethod
     public void setup9(){
-        System.out.println("Mohoshi Process-9");
+        System.out.println("After Method-2");
     }
 
 }
